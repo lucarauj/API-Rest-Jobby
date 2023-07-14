05ad5e3a59d602031df4a20be79dee58fb8ceeaf
JGMelon22 <jgvasconcellos22@gmail.com>
2023-07-14 20:11:24 -0300

    FIX: corrigindo a strategy para usar o tipo "IDENTITY" ao em vez de AUTO devido a um problema de insert (POST)

--- jobby/src/main/java/com/jobby/g6/domain/model/CadastroExperiencia.java
+++ jobby/src/main/java/com/jobby/g6/domain/model/CadastroExperiencia.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 public class CadastroExperiencia {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Integer id;
 
