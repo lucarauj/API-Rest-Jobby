c9029c44534090a3d8b8654d8155d5768d302d47
JGMelon22 <jgvasconcellos22@gmail.com>
2023-07-14 19:41:31 -0300

    Feat: adicionando migration "V002__criacao-indices" para criar indexes nas tabelas

---
+++ jobby/src/main/resources/db/migration/V002__criacao-indices.sql
@@ -0,0 +1,17 @@
+create index idx_cadastros
+ON cadastro(id);
+
+create index cadastro_experiencias
+ON cadastro_experiencia(id);
+
+create index idx_cidades
+ON cidade(id);
+
+create index idx_empresas
+ON empresa(id);
+
+create index idx_pretensoes_salariais
+ON pretensao_salarial(id);
+
+create index idx_profissoes
+ON profissao(id);
