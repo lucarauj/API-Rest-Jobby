e0dcff08ee32654d08494551e72b670154bdb3a7
JGMelon22 <jgvasconcellos22@gmail.com>
2023-07-14 20:02:04 -0300

    Feat: adicionando método de pesquisa por todos os cadastros na service e invocando na controller de cadastros

--- jobby/src/main/java/com/jobby/g6/JobbyG6Application.java
+++ jobby/src/main/java/com/jobby/g6/JobbyG6Application.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JobbyG6Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JobbyG6Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(JobbyG6Application.class, args);
+    }
 }
